e3c2d89ec5f4cd92de964a06b489472360cd95d4
Divyam6255 <divyam21btai10@karnavatiuniversity.edu.in>
2025-03-28 12:21:46 +0530

    Added a path to the code

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.get("/greet/:name", (req, res) => {
   res.json({ message: `Hello, ${req.params.name}!` });
 });
 
+app.get("/time", (req, res) => {
+    const currentTime = new Date().toISOString();
+    res.json({ time: currentTime });
+});
+
 // Export Lambda Handler
 const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
